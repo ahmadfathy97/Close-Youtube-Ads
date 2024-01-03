c54165392f30b1720d72f67394a396dd3bd5973e
Ahmad Fathy <af1381@fayoum.edu.eg>
2024-01-04 00:27:52 +0200

    Update class name

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function closeAd(){
   setInterval(function () {
-    let closeBtn = document.querySelector('.ytp-ad-overlay-close-button')
-    let skipBtn = document.querySelector('.ytp-ad-skip-button')
+    let closeBtn = document.querySelector('.ytp-ad-overlay-close-button-modern')
+    let skipBtn = document.querySelector('.ytp-ad-skip-button-modern')
     if(closeBtn){
       closeBtn.click();
     }
